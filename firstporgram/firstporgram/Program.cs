8a8268e1a8d57ea72ade1400782c798497e9d85d
Ali <Ali@DESKTOP-2PO84FL>
2017-04-12 16:10:33 +0200

    first c# porgram

---
+++ firstporgram/firstporgram/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstporgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is my first porgram in c#
+            Console.WriteLine("hello world");
+            Console.ReadKey(); 
+        }
+    }
+}
