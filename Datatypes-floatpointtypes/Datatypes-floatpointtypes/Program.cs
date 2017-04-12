5cf674f4fc0150e7ce6fc7a76f3722e753a11dff
Ali <Ali@DESKTOP-2PO84FL>
2017-04-12 22:04:03 +0200

    datatypes-floatpointtypes

---
+++ Datatypes-floatpointtypes/Datatypes-floatpointtypes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datatypes_floatpointtypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float flmin = float.MinValue;
+            float flmax = float.MaxValue;
+            Console.WriteLine("min value = {0} max value = {1}", flmin, flmax);
+
+
+
+
+
+
+         float flVar = 1F / 3;
+            double dlVar = 1D / 3;
+            decimal dcVar = 1M / 3;
+
+
+
+
+
+
+
+            Console.WriteLine("\n float value = {0}\n double value = {1}\n decimal value = {2}", flVar , dlVar , dcVar);
+
+            Console.ReadKey();
+
+        }
+    }
+}
