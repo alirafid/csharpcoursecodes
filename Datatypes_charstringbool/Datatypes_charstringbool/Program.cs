c79216f87cea2df56391e2b50433c2f9999dce6a
Ali <Ali@DESKTOP-2PO84FL>
2017-04-13 16:54:55 +0200

    Datatypes_charstringbooltypes

---
+++ Datatypes_charstringbool/Datatypes_charstringbool/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datatypes_charstringbool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int charmain = char.MinValue;
+                int charmax = char.MaxValue;
+
+            char myvar = 'a';
+            int myvarvalue = 'a';
+            char myvarvalue2 = Convert.ToChar(myvarvalue);
+
+
+          
+                 Console.WriteLine("Min Char = {0} Max Char = {1} My Charctar = {2} My variable number = {3} my converison value = {4}", charmain, charmax, myvar, myvarvalue , myvarvalue2);
+           
+             string myString = (" this is my charctar a value 97 and this my charctar A value 75 ");
+            Console.WriteLine("\nMy String = {0}", myString);
+
+
+            bool myBool = true;
+            Console.WriteLine("\n my boolean value is {0} ", myBool);
+             Console.ReadKey();
+
+
+        }
+    }
+}
